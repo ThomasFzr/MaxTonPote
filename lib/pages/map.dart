c10a6f070812e3741d6b03d5ad6a0da5d0c3070c
ThomasFzr <thfltzr@gmail.com>
2025-02-18 15:01:22 +0100

    Add location permission description, disable debug banner, and implement map annotations

--- lib/pages/map.dart
+++ lib/pages/map.dart
@@ -1,22 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:mapbox_maps_flutter/mapbox_maps_flutter.dart';
 
-class MapPage extends StatelessWidget {
+class MapPage extends StatefulWidget {
   const MapPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    final camera = CameraOptions(
-      center: Point(coordinates: Position(-98.0, 39.5)),
-      zoom: 2,
-    );
+  _MapPageState createState() => _MapPageState();
+}
 
-    return MaterialApp(
-      home: Scaffold(
-        body: MapWidget(
-          cameraOptions: camera,
+class _MapPageState extends State<MapPage> {
+  late MapboxMap mapboxMap;
+  PointAnnotationManager? pointAnnotationManager;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: MapWidget(
+        cameraOptions: CameraOptions(
+          center: Point(coordinates: Position(-98.0, 39.5)), // Corrected
+          zoom: 2,
+          
         ),
+        onMapCreated: _onMapCreated,
       ),
     );
+  }
+
+  Future<void> _onMapCreated(MapboxMap map) async {
+    mapboxMap = map;
+    pointAnnotationManager = await map.annotations.createPointAnnotationManager();
+
+    try {
+      final ByteData bytes = await rootBundle.load('assets/mark.png');
+      final Uint8List imageData = bytes.buffer.asUint8List();
+
+      PointAnnotationOptions options = PointAnnotationOptions(
+        geometry: Point(coordinates: Position(-74.00913, 40.75183)), // Example location
+        image: imageData,
+        iconSize: 0.2,
+      );
+
+      await pointAnnotationManager?.create(options);
+    } catch (e) {
+      debugPrint("Error loading image: $e");
+    }
   }
 }
