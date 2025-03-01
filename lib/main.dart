63af033db314aaafb01833e9f1637c12039ca20a
ThomasFzr <thfltzr@gmail.com>
2025-03-01 16:00:15 +0100

    Add Firebase configuration files for Android and iOS

--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,15 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 import 'pages/home.dart';
 import 'pages/map.dart';
 import 'pages/profile.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  await Firebase.initializeApp(
+  options: DefaultFirebaseOptions.currentPlatform,
+);
   await dotenv.load(fileName: '.env');
   String accessToken = dotenv.get("SK_MAPBOX_TOKEN");
   MapboxOptions.setAccessToken(accessToken);
